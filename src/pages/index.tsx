34f20337cf897d9eaafada1d86013ec61115125f
AsToNlele <theastonofficial@gmail.com>
2023-06-26 20:02:20 +0200

    feat: nottweeting input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
-import { Twitter, X, Home as HomeIcon } from "lucide-react";
+import { Home as HomeIcon, Twitter, X } from "lucide-react";
 import Head from "next/head";
+import { type FocusEvent, useCallback, useState } from "react";
+import sanitizeHtml from "sanitize-html";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
 
 export default function Home() {
+  const [content, setContent] = useState<string>("");
+
+  const onContentBlur = useCallback((evt: FocusEvent<HTMLInputElement>) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
+    setContent(sanitizeHtml(evt.target.innerHTML));
+  }, []);
   return (
     <>
       <Head>
@@ -11,18 +20,20 @@ export default function Home() {
       <main>
         <div className="flex">
           <div className="flex shrink-0 grow basis-auto flex-col items-end">
-            <div className="flex h-screen w-[275px] flex-col justify-between py-4">
+            <div className="flex h-screen w-[200px] flex-col justify-between py-4">
               <div className="flex flex-col gap-5">
                 <div className="h-8">
-                  <div className="relative">
-                    <Twitter
-                      size={30}
-                      strokeWidth={1}
-                      fill="white"
-                      className="absolute"
+                  <Twitter
+                    size={30}
+                    strokeWidth={1}
+                    fill="white"
+                    className="relative"
+                  >
+                    <X
+                      color="red"
+                      className="absolute left-0 top-0 h-full w-full"
                     />
-                    <X size={30} color="red" className="absolute" />
-                  </div>
+                  </Twitter>
                 </div>
                 <div className="flex">
                   <div className="items-left flex flex-col">
@@ -52,7 +63,35 @@ export default function Home() {
           </div>
           <div className="flex shrink grow items-start">
             <div className="w-[900px]">
-              <div className="h-screen w-[650px] break-all border-x">Feed</div>
+              <div className="h-screen w-[650px] break-all border-x">
+                <div className="border-b p-4">
+                  <span className="text-xl font-semibold">Home</span>
+                </div>
+                <div className="border-b p-4">
+                  <div className="flex w-full flex-col gap-4">
+                    <div className="flex gap-4">
+                      <Avatar className="h-10 w-10">
+                        <AvatarImage
+                          src="https://github.com/astonlele.png"
+                          alt="@shadcn"
+                        />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <div
+                        className="block w-full resize-none pt-1 text-xl outline-none focus:border-transparent focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
+                        role="textbox"
+                        contentEditable
+                        data-placeholder="What is happening?!"
+                        onBlur={onContentBlur}
+                        dangerouslySetInnerHTML={{ __html: content }}
+                      />
+                    </div>
+                    <div className="flex justify-end">
+                      <Button>Tweet</Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
