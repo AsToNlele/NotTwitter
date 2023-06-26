35cae7616928e8f45e10ab8e2d662ea3f34499c1
AsToNlele <theastonofficial@gmail.com>
2023-06-26 23:36:15 +0200

    feat: user profile dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+} from "@radix-ui/react-dropdown-menu";
 import { Home as HomeIcon, Twitter, X } from "lucide-react";
 import Head from "next/head";
 import { type FocusEvent, useCallback, useState } from "react";
 import sanitizeHtml from "sanitize-html";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
+import {
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 
 export default function Home() {
   const [content, setContent] = useState<string>("");
@@ -20,9 +30,9 @@ export default function Home() {
       <main>
         <div className="flex">
           <div className="hidden shrink basis-auto flex-col items-end xs:flex md:shrink-0 md:grow">
-            <div className="flex h-screen w-[100px] flex-col justify-between py-4 lg:w-[200px]">
+            <div className="flex h-screen w-[100px] flex-col justify-between py-2 lg:w-[200px]">
               <div className="flex flex-col gap-5">
-                <div className="flex h-8 justify-center lg:justify-normal">
+                <div className="flex h-8 justify-center px-4 py-2 lg:justify-normal">
                   <Twitter
                     size={30}
                     strokeWidth={1}
@@ -35,25 +45,37 @@ export default function Home() {
                     />
                   </Twitter>
                 </div>
-                <div className="items-left flex items-center justify-center gap-4 lg:justify-normal">
+                <div className="items-left flex items-center justify-center gap-4 p-4 lg:justify-normal">
                   <HomeIcon size={30} />
                   <span className="hidden text-xl font-semibold text-primary lg:block">
                     Home
                   </span>
                 </div>
               </div>
-              <div className="flex justify-center gap-3 lg:justify-normal">
-                <Avatar className="h-10 w-10">
-                  <AvatarImage
-                    src="https://github.com/astonlele.png"
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className="hidden flex-col text-sm lg:flex ">
-                  <span className="font-semibold">AsToN</span>
-                  <span className="text-slate-500">@AsToN</span>
-                </div>
+              <div className="flex">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="flex w-full justify-center gap-3 rounded-md px-4 py-2 hover:bg-accent lg:mr-2 lg:justify-normal">
+                    <div className="flex justify-center gap-3 lg:justify-normal">
+                      <Avatar className="h-10 w-10">
+                        <AvatarImage
+                          src="https://github.com/astonlele.png"
+                          alt="@shadcn"
+                        />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <div className="hidden flex-col text-sm lg:flex">
+                        <span className="font-semibold">AsToN</span>
+                        <span className="text-slate-500">@AsToN</span>
+                      </div>
+                    </div>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-24 rounded-md border p-1 lg:w-48">
+                    <DropdownMenuLabel>Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator className="mx-0" />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Logout</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </div>
             </div>
           </div>
