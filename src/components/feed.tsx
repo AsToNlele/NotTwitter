9cdd3b25b8306323d8065234cd4bcb5f6d3ddd36
AsToNlele <theastonofficial@gmail.com>
2023-06-29 23:41:35 +0200

    feat: mobile bottom navbar

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -1,12 +1,14 @@
 import { FeedTop } from "./feed-top";
 import { Tweeter } from "./tweeter";
 import { Tweets } from "./tweets";
+import { FeedBottomMobile } from "./feed-bottom-mobile";
 
 export const Feed = () => {
   return (
     <div className="flex grow items-start md:grow">
       <div className="w-full xl:w-[900px]">
         <div className="min-h-screen w-full break-all border-none xs:border-l xs:border-solid md:w-[650px] md:border-x ">
+          <FeedBottomMobile />
           <FeedTop />
           <Tweeter />
           <Tweets />
