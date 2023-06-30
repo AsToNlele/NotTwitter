0e9b0ef9b4618b107da294c69c2481c61b8df7c5
AsToNlele <theastonofficial@gmail.com>
2023-06-30 19:30:17 +0200

    fix: mobile nav background

--- src/components/feed-bottom-mobile.tsx
+++ src/components/feed-bottom-mobile.tsx
@@ -1,7 +1,7 @@
 import { Home, Search, Bell, Mail } from "lucide-react";
 
 export const FeedBottomMobile = () => (
-  <div className="fixed bottom-0 z-10 flex w-full items-center justify-evenly gap-6 border-t bg-black py-4 xs:hidden">
+  <div className="fixed bottom-0 z-10 flex w-full items-center justify-evenly gap-6 border-t bg-background py-4 xs:hidden">
     <Home className="h-7 w-7" />
     <Search className="h-7 w-7" />
     <Bell className="h-7 w-7" />
