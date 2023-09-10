a7635aef69c7b6fe0c350c2e229322499f2723b8
AsToNlele <theastonofficial@gmail.com>
2023-09-10 23:55:22 +0200

    feat: comment modal

--- src/components/tweeter-dialog.tsx
+++ src/components/tweeter-dialog.tsx
@@ -2,12 +2,46 @@ import { Dialog, DialogTrigger, DialogContent } from "./ui/dialog";
 import { Edit } from "lucide-react";
 import { Tweeter } from "./tweeter";
 import useTweeterDialog from "~/hooks/useTweeterDialog";
+import { tweetFeedDate } from "~/components/tweets";
+import { MyAvatar } from "~/components/my-avatar";
+import { UserTag } from "~/components/user-tag";
+import type { TweetWithAuthorAndLikes } from "prisma/customTypes";
+import { Separator } from "~/components/ui/separator";
+
+const TweetPreview = ({ tweet }: { tweet: TweetWithAuthorAndLikes }) => {
+  return (
+    <div className="flex gap-4 pt-8">
+      <div className="flex flex-col">
+        <MyAvatar image={tweet.author.image} />
+        <Separator orientation="vertical" className="mx-5 max-h-8" decorative />
+      </div>
+      <div className="flex flex-1 flex-col gap-0 hover:cursor-default">
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1" id={tweet.id}>
+            <UserTag user={tweet.author} horizontal />
+            <span className="text-slate-500">·</span>
+            <span className="text-sm text-slate-500">
+              {tweetFeedDate(tweet.createdAt)}
+            </span>
+            <div id={tweet.id} className="flex-1 hover:cursor-pointer">
+              &shy;
+            </div>
+          </div>
+        </div>
+        <div className="flex" id={tweet.id}>
+          <span className="whitespace-pre-wrap">{tweet.text}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const TweeterDialog = () => {
-  const { open, setOpen } = useTweeterDialog((state) => {
+  const { open, setOpen, replyTo } = useTweeterDialog((state) => {
     return {
       open: state.open,
       setOpen: state.setOpen,
+      replyTo: state.replyTo,
     };
   });
   return (
@@ -18,8 +52,13 @@ export const TweeterDialog = () => {
             <Edit className="h-7 w-7" />
           </div>
         </DialogTrigger>
-        <DialogContent className="h-screen w-full sm:h-auto sm:max-h-screen">
-          <Tweeter isModal />
+        <DialogContent className="w-full sm:h-auto sm:max-h-screen">
+          {replyTo && open && <TweetPreview tweet={replyTo} />}
+          <Tweeter
+            isModal
+            isReply={replyTo ? true : false}
+            replyTweetId={replyTo?.id}
+          />
         </DialogContent>
       </Dialog>
     </div>
--- src/hooks/useTweeterDialog.tsx
+++ src/hooks/useTweeterDialog.tsx
@@ -1,12 +1,18 @@
+import type { TweetWithAuthorAndLikes } from "prisma/customTypes";
 import { create } from "zustand";
 
 interface TweeterDialog {
   open: boolean;
   setOpen: (open: boolean) => void;
+  replyTo: TweetWithAuthorAndLikes | null;
+  setOpenWithComment: (replyTo: TweetWithAuthorAndLikes | null) => void;
 }
 const useTweeterDialog = create<TweeterDialog>()((set) => ({
   open: false,
-  setOpen: (open: boolean) => set({ open }),
+  setOpen: (open: boolean) => set({ open, replyTo: null }),
+  replyTo: null,
+  setOpenWithComment: (replyTo: TweetWithAuthorAndLikes | null) =>
+    set({ replyTo, open: true }),
 }));
 
 export default useTweeterDialog;
