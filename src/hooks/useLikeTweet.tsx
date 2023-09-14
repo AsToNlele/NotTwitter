f6bd9da633320e1582a63a951eee6f31de026df7
AsToNlele <theastonofficial@gmail.com>
2023-09-14 16:56:29 +0200

    fix: optimistic update

--- src/hooks/useLikeTweet.tsx
+++ src/hooks/useLikeTweet.tsx
@@ -1,36 +1,71 @@
-import type { TweetWithAuthorAndLikes } from "prisma/customTypes";
+import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 import { optimisticallyChangeLike } from "~/utils/tweet";
 
 const useLikeTweet = () => {
   const utils = api.useContext();
+  const router = useRouter();
+  const { tweet } = router.query;
+  const tweetId = Array.isArray(tweet) ? tweet[0] : tweet;
+
   const likeOrUnlikeTweet = api.like.like.useMutation({
     onMutate: async (value) => {
       // Optimistically like/unlike tweet
       await utils.tweet.getAll.cancel();
-      utils.tweet.getAll.setData(undefined, (oldTweets) => {
-        if (!oldTweets) return undefined;
-        const newTweets = oldTweets.map((tweet) => {
-          if (tweet.id === value?.tweetId) {
-            return optimisticallyChangeLike(tweet);
-          } else {
-            return tweet;
-          }
-        });
-        return newTweets;
-      });
-      await utils.tweet.getOne.cancel({ tweet: value?.tweetId });
-      utils.tweet.getOne.setData(
-        { tweet: value?.tweetId },
-        (oldTweet): TweetWithAuthorAndLikes | undefined => {
-          if (!oldTweet) return undefined;
-          return optimisticallyChangeLike(oldTweet);
-        }
+      utils.tweet.getAll.setData(
+        undefined,
+        (oldTweets) =>
+          oldTweets?.map((tweet) =>
+            tweet.id === value?.tweetId
+              ? optimisticallyChangeLike(tweet)
+              : tweet,
+          ),
       );
+      await utils.tweet.getOne.cancel({ tweet: value?.tweetId });
+      utils.tweet.getOne.setData({ tweet: value?.tweetId }, (oldTweet) =>
+        oldTweet ? optimisticallyChangeLike(oldTweet) : oldTweet,
+      );
+
+      if (tweetId) {
+        // Optimistically update comments
+        await utils.tweet.getComments.cancel({ tweet: tweetId });
+        utils.tweet.getComments.setData(
+          { tweet: tweetId },
+          (oldComments) =>
+            oldComments?.map((comment) =>
+              comment.id === value?.tweetId
+                ? optimisticallyChangeLike(comment)
+                : comment,
+            ),
+        );
+
+        // Optimistically update single tweet and parent tweets
+        utils.tweet.getOneWithParents.setData({ tweet: tweetId }, (oldData) =>
+          oldData && oldData.tweet
+            ? oldData.tweet.id === value?.tweetId
+              ? // Main tweet
+                {
+                  tweet: optimisticallyChangeLike(oldData.tweet),
+                  parentTweets: oldData.parentTweets,
+                }
+              : // Parent tweets
+                {
+                  tweet: oldData.tweet,
+                  parentTweets: oldData.parentTweets.map((parentTweet) =>
+                    parentTweet.id === value?.tweetId
+                      ? optimisticallyChangeLike(parentTweet)
+                      : parentTweet,
+                  ),
+                }
+            : oldData,
+        );
+      }
     },
     onSettled: (value) => {
       utils.tweet.getAll.invalidate();
       utils.tweet.getOne.invalidate({ tweet: value?.tweetId });
+      utils.tweet.getComments.invalidate({ tweet: tweetId });
+      utils.tweet.getOneWithParents.invalidate({ tweet: tweetId });
     },
   });
 
