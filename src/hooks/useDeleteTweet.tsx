3679de2db6cbf4fe8f2c35b33d115353993f5529
AsToNlele <theastonofficial@gmail.com>
2023-09-17 00:41:55 +0200

    feat: delete tweet optimistic update

--- src/hooks/useDeleteTweet.tsx
+++ src/hooks/useDeleteTweet.tsx
@@ -1,7 +1,37 @@
+import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 
 const useDeleteTweet = () => {
-  const deleteTweet = api.tweet.delete.useMutation();
+  const router = useRouter();
+  const { tweet } = router.query;
+  const tweetId = Array.isArray(tweet) ? tweet[0] : tweet;
+  const utils = api.useContext();
+  const deleteTweet = api.tweet.delete.useMutation({
+    onMutate: async (value) => {
+      await utils.tweet.getAll.cancel();
+      utils.tweet.getAll.setData(
+        undefined,
+        (oldTweets) =>
+          oldTweets?.filter((tweet) => tweet.id !== value?.tweet) || [],
+      );
+
+      if (tweetId) {
+        await utils.tweet.getComments.cancel({ tweet: tweetId });
+        utils.tweet.getComments.setData(
+          { tweet: tweetId },
+          (oldComments) =>
+            oldComments?.filter((comment) => comment.id !== value?.tweet) || [],
+        );
+      }
+    },
+    onSettled: async (value) => {
+      if (tweetId && tweet === value?.id) {
+        await router.push("/");
+      }
+      await utils.tweet.getAll.invalidate();
+      await utils.tweet.getComments.invalidate({ tweet: tweetId });
+    },
+  });
   return deleteTweet;
 };
 
