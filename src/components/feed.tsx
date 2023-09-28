0e8f3f80d906122c61199314119c413fbd0a68a6
AsToNlele <theastonofficial@gmail.com>
2023-09-28 18:00:28 +0200

    refactor: move tweeterdialog in layout

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -1,6 +1,5 @@
 import { FeedTop } from "./feed-top";
 import { Tweeter } from "./tweeter";
-import { TweeterDialog } from "./tweeter-dialog";
 import { Tweets } from "./tweets";
 
 export const Feed = () => {
@@ -8,7 +7,6 @@ export const Feed = () => {
     <div className="flex grow items-start md:grow">
       <div className="w-full xl:w-[900px]">
         <div className="flex min-h-screen w-full flex-col break-all border-none xs:border-l xs:border-solid md:w-[650px] md:border-x ">
-          <TweeterDialog />
           <FeedTop />
           <Tweeter />
           <Tweets />
