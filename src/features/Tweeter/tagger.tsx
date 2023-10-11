3ff1dfc16c9153085d3af03054f4827504ce697a
AsToNlele <theastonofficial@gmail.com>
2023-10-11 18:53:46 +0200

    fix: tag styles

--- src/features/Tweeter/tagger.tsx
+++ src/features/Tweeter/tagger.tsx
@@ -16,14 +16,18 @@ const Tagger = memo(function Tagger({
       <div
         className={`${
           data && data?.length > 0 ? "block" : "hidden"
-        } bg-red absolute left-0 top-0 h-full w-full bg-opacity-50`}
+        } absolute left-0 top-0 h-full w-full bg-primary bg-opacity-50`}
       >
-        <ScrollArea className="bg-red h-32 w-[50%] rounded-md border">
-          <div className="flex flex-col gap-2 bg-red-400 p-4">
+        <ScrollArea
+          className={`bg-red ${
+            data && data?.length > 1 ? "h-32" : "h-16"
+          } w-[50%] rounded-md border`}
+        >
+          <div className="flex flex-col bg-background pr-3">
             {data?.map((d) => (
               <div
                 key={d.handle}
-                className="flex gap-2 hover:bg-green-400"
+                className="flex gap-2 p-2 hover:bg-accent"
                 onClick={() => selectHandle(d.handle!)}
               >
                 <MyAvatar image={d.image} />
